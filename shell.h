1dd37ce3c72b111b85470a1ce1c59693a51d6cec
Martin <martymalkav7@gmail.com>
2023-08-11 19:49:38 +0000

    shell.h

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+/*
+*Allowed functions
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+#endif
