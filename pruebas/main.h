0dab873bbedba7da83602f53107a477a37ee232f
gastonperego <gastonperego@holbertonstudents.com>
2023-08-12 17:22:16 +0000

    feat: main.h, hsh && tokenizer

---
+++ pruebas/main.h
@@ -0,0 +1,9 @@
+#ifndef HEADERFILE
+#define HEADERFILE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <string.h>
+char **tokenizer(char *input);
+#endif
