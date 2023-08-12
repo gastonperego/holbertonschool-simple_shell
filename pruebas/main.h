6a447d9164e32887292956d1c7f7b0f0dc1f98f6
Gastón <135636714+gastonperego@users.noreply.github.com>
2023-08-12 14:24:09 -0300

    Delete pruebas directory

--- pruebas/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADERFILE
-#define HEADERFILE
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stddef.h>
-#include <string.h>
-char **tokenizer(char *input);
-#endif
