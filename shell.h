7e60609fdf1f29caf9ef5e0783135e3ab3541dc1
Martin <martymalkav7@gmail.com>
2023-08-18 16:47:28 +0000

    fix: exit when first argument is the path to the command and memory errors

--- shell.h
+++ shell.h
@@ -35,7 +35,7 @@ void free_dp(char **command);
 void free_exit(char **command);
 char **tokening(char *input, char *delim);
 int change_dir(const char *path);
-void forker(char **cmd);
+void forker(char **cmd, int *exit_status);
 void forker2(char **command, char **path);
 
 #endif
